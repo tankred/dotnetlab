927caca670c8073ab6a62aadb214ca19a9fcb95a
Kurt L. Duyck <kurt.duyck@protonmail.com>
2024-10-18 15:31:17 +0200

    Generate a sample excel file
    *	modified:   .gitignore
    *	modified:   CHANGELOG.md
    *	modified:   HelloWorldConsole/HelloWorldConsole.csproj
    *	modified:   HelloWorldConsole/Program.cs

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ClosedXML.Excel; // REF closedXML package ! 
+using var workbook = new XLWorkbook();
+// See https://aka.ms/new-console-template for more information
+var worksheet = workbook.AddWorksheet("Sample Sheet");
+worksheet.Cell("A1").Value = "Hello World!";
+worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
+workbook.SaveAs("HelloWorld.xlsx");
 Console.WriteLine("Hello, World!");
+
