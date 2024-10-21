3d21b15e718a96e7a08bd297daf89f347eb647d2
Kurt L. Duyck <kurt.duyck@protonmail.com>
2024-10-21 11:25:46 +0200

    Create report dir for exports
    *	modified:   .gitignore
    *	modified:   CHANGELOG.md
    *	modified:   HelloWorldConsole/Program.cs
    *	modified:   README.md

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,9 +1,9 @@
 ﻿using ClosedXML.Excel; // REF closedXML package ! 
 using var workbook = new XLWorkbook();
-// See https://aka.ms/new-console-template for more information
 var worksheet = workbook.AddWorksheet("Sample Sheet");
 worksheet.Cell("A1").Value = "Hello World!";
 worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
-workbook.SaveAs("HelloWorld.xlsx");
-Console.WriteLine("Hello, World!");
-
+workbook.SaveAs("../report/HelloWorld.xlsx");
+//
+// Console.WriteLine("Hello, World!");
+// 
