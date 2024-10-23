2fc9becaed558155e0d7b1ce46ac389d6cf0a439
Kurt L. Duyck <kurt.duyck@protonmail.com>
2024-10-23 17:30:02 +0200

    Update workbook using ClosedXML
    *	modified:   CHANGELOG.md
    *	modified:   HelloWorldConsole/Program.cs

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,6 +1,6 @@
 ﻿using ClosedXML.Excel; // REF closedXML package ! 
 using var workbook = new XLWorkbook();
-var worksheet = workbook.AddWorksheet("Sample Sheet");
+var worksheet = workbook.AddWorksheet("TABBLAD-891");
 worksheet.Cell("A1").Value = "Hello World!";
 worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
 workbook.SaveAs("../report/HelloWorld.xlsx");
