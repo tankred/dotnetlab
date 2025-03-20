5d3da8a479e2ff4931e6206fd4a3354d4e8a573c
Kurt L. Duyck <kurt.duyck@protonmail.com>
2025-03-20 15:42:15 +0100

    Update console app - rollback to hello world
    *	modified:   CHANGELOG.md
    *	modified:   HelloWorldConsole/Program.cs

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,9 +1,9 @@
-﻿using ClosedXML.Excel; // REF closedXML package ! 
-using var workbook = new XLWorkbook();
-var worksheet = workbook.AddWorksheet("TABBLAD-891");
-worksheet.Cell("A1").Value = "Hello World!";
-worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
-workbook.SaveAs("../report/HelloWorld.xlsx");
+﻿// using ClosedXML.Excel; // REF closedXML package ! 
+// using var workbook = new XLWorkbook();
+// var worksheet = workbook.AddWorksheet("TABBLAD-891");
+// worksheet.Cell("A1").Value = "Hello World!";
+// worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
+// workbook.SaveAs("../report/HelloWorld.xlsx");
 //
-// Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 // 
